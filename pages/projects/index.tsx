502f8ec198e262aba33499d9c7de2fb98b4e8d50
Saif <92519922+SA12IF34@users.noreply.github.com>
2023-02-12 07:27:42 +0300

    first commit

---
+++ pages/projects/index.tsx
@@ -0,0 +1,13 @@
+
+
+const Projects = () => {
+
+    return (
+        <>
+            <h1>hello in projects page</h1>
+        </>
+    )
+}
+
+
+export default Projects;
---
+++ pages/projects/squeez-page.tsx
@@ -0,0 +1,13 @@
+
+
+const Squeez = () => {
+    
+    return (
+        <>
+            <h1>Welcome to my squeeze</h1>
+        </>
+    )
+}
+
+
+export default Squeez;
